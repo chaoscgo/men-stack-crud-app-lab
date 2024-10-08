b0bf5e6518b2725300a2ac627fb78afc5b0d2ed8
Beverly <chaoscgo@hotmail.com>
2024-10-08 12:34:59 -0500

    Added route and view for Create operation

--- server.js
+++ server.js
@@ -15,12 +15,24 @@ mongoose.connection.on("connected", () => {
 
 const Book = require('./models/book.js');
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
 app.get('/books/new', (req, res) => {
     res.render('books/new.ejs');
+});
+
+app.post('/books', async (req, res) => {
+    if (req.body.recommendation === 'on') {
+        req.body.recommendation = true;
+    } else {
+        req.body.recommendation = false;
+    }
+    await Book.create(req.body);
+    res.redirect('/books/new');
 });
 
 app.listen(3000, () => {
